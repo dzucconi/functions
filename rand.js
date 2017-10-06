62b89ef186840123f1cdf07127b86347bb6445a9
Damon <mail@damonzucconi.com>
2017-10-06 15:21:59 -0400

    Update rand.js

--- rand.js
+++ rand.js
@@ -1,2 +1,8 @@
-export default (min, max) =>
+const rand = (min, max) =>
   Math.random() * (max - min) + min;
+
+export const int = (min, max) =>
+  Math.floor(Math.random() * (max - min)) + min;
+
+export const inclusive = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
